6a1e15c2b1564735b8a0e8408ee977e05d242488
msinans <msomercikoglu@gmail.com>
2022-06-21 21:27:35 +0300

    21.06.2022

--- Ders08Classlar/Program.cs
+++ Ders08Classlar/Program.cs
@@ -1,13 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-
+// using Ders08Classlar;
 public class Ev // C# ta class tanımlama
 {
     public int KapiNo; // Sınıf değişkenleri oluşturma
     public string SokakAdi;
 }
 
-namespace MyApp // Note: actual namespace depends on the project name.
+namespace Ders08Classlar // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
@@ -38,10 +37,33 @@ namespace MyApp // Note: actual namespace depends on the project name.
                 EgitimAdi = "Yazılım Uzmanlığı",
                 KapiNo = 102,
                 SinifMevcudu = 6
+             
             };
 
-            Console.WriteLine("Kurs Bilgilerim \n : Kurs Adı {0} Eğitim Adı : {1} Kapı No : {2}", kurs.KursAdi, kurs.EgitimAdi, kurs.KapiNo);
+            Console.WriteLine("Kurs Bilgilerim : \n Kurs Adı {0} Eğitim Adı : {1} Kapı No : {2}", kurs.KursAdi, kurs.EgitimAdi, kurs.KapiNo);
 
+            Araba araba = new()
+            {
+                KasaTipi = "Sedan",
+                Marka = "Fiat",
+                Model = "Linea",
+                Renk = "Beyaz",
+                UretimYili = 2013
+            };
+            Console.WriteLine($"Araba Bilgileriniz :\nMarka : {araba.Marka} \nModel : {araba.Model}\nRenk : {araba.Renk}" );
+
+            Urun urun = new()
+            {
+                Fiyat = 99,
+                Ozellikler = "Işıklı Klavye",
+                Stok = 10,
+                UrunAdi = "Oyuncu Klavye"
+            };
+            Console.WriteLine("Ürün Bilgileri");
+            Console.WriteLine($"Ürün Adı : {urun.UrunAdi }");
+            Console.WriteLine($"Ürün Fiyatı : {urun.Fiyat}");
+            Console.WriteLine($"Ürün Stok : {urun.Stok}");
+            Console.WriteLine($"Ürün Özellikler : {urun.Ozellikler}");
         }
     }
 }
@@ -52,4 +74,13 @@ public class Kurs
     public string KursAdi;
     public string EgitimAdi;
     public int SinifMevcudu;
+}
+
+public class Araba
+{
+    public string Marka;
+    public string Model;
+    public string Renk;
+    public string KasaTipi;
+    public int UretimYili;
 }
---
+++ Ders08Classlar/Urun.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ders08Classlar
+{
+    internal class Urun
+    {
+        public string UrunAdi;
+        public int Stok;
+        public decimal Fiyat;
+        public string Renk;
+        public string Ozellikler;
+
+
+
+
+
+
+
+
+    }
+}
